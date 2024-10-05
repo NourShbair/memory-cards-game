8a740d8b9fcc7c917de5548a3db1e326efd72ad9
Nour <nshbair@gmail.com>
2024-10-05 15:45:26 +0100

    Draw game cards dynamically using js and bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
+//access the game div
 const gameBoard = document.querySelector("#game");
 
+//this function draw the cards on the screen depending on specific number as parameter, and use of css grid system to handle responsivness
 function drawCards(number) {
+    let cardsContainer = document.createElement("div");
+    //add bootstrap classes to the container div
+    cardsContainer.classList.add("container", "text-center");
+    gameBoard.appendChild(cardsContainer);
+    let row = document.createElement("div");
+    //add bootstrap classes to row div
+    row.classList.add("row", "row-cols-2", "row-cols-lg-4","g-2", "g-lg-3");
+    cardsContainer.appendChild(row);
     const cardsCount = 2 * number;
     for (let i = 0; i < cardsCount; i++) {
+        let column = document.createElement("div");
+        //add bootstrap class to column div
+        column.classList.add("col");
+        row.appendChild(column);
         let card = document.createElement("div");
-        card.classList.add("square", "uflipped");
-        gameBoard.appendChild(card);
+        //add classes to card div and implement them in css file
+        card.classList.add("p-3", "square", "unflipped");
+        column.appendChild(card);
     }
-
 }
 
 drawCards(4);
