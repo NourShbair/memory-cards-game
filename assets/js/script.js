92af3610dc2cdbfe4812461b16df4bb8d4008e66
Nour <nshbair@gmail.com>
2024-10-05 23:28:05 +0100

    Update icon selection algorithm

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@ const gameBoard = document.querySelector("#game");
 let cardsArray = [];
 
 //declare imagesArray
-let imagesArray = ["🍉","🍓","🥝","🍏","🍇","🍒","🍑","🫐","🍐","🍍"];
+let imagesArray = ["🍉", "🍓", "🥝", "🍏", "🍇", "🍒", "🍑", "🫐", "🍐", "🍍"];
 
 //declare numberOfImages
-let numberOfImages = 10;
+let numberOfImages = 4;
 
 //this function draw the cards on the screen depending on specific number as parameter, and use of css grid system to handle responsivness
 function drawCards(number) {
@@ -50,32 +50,49 @@ function flip(card) {
     cardDiv.classList.toggle("flipped");
 }
 
+
 //shuffle the images and distribute them on cards
 function distributeImages() {
+
+    //initialize and declare index array 
+    let indexArray = [];
+    for (let i = 0; i < cardsArray.length; i++) {
+        indexArray.push(i);
+    }
+    shuffle(indexArray);
+
     //... is dereferencing for the array before concatination
-    let doubledImagesArray = [...imagesArray].concat(...imagesArray)
-    shuffle(doubledImagesArray)
-    for(let i = 0; i < doubledImagesArray.length; i++){
-        cardsArray[i].textContent=doubledImagesArray[i];
+    let doubledImagesArray = [...imagesArray].concat(...imagesArray);
+    shuffle(doubledImagesArray);
+
+    for (let i = 0; i < numberOfImages; i++) {
+        let shuffledIndex1 = Number(indexArray[i]);
+        let shuffledIndex2 = Number(indexArray[i + numberOfImages]);
+        console.log(shuffledIndex1);
+        console.log(shuffledIndex2);
+        console.log(cardsArray);
+        cardsArray[shuffledIndex1].textContent = doubledImagesArray[i];
+        cardsArray[shuffledIndex2].textContent = doubledImagesArray[i];
+
     }
 }
 
 
 function shuffle(array) {
     let currentIndex = array.length;
-  
+
     // While there remain elements to shuffle...
     while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
     }
-  }
+}
 
 drawCards(numberOfImages);
 distributeImages();
